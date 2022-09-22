c7d46fe9594936fabf30a4a1f35a703c1ba3a674
LilyPr <lilyeginald09@gmail.com>
2022-09-22 09:58:13 -0700

    list argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
+if __name__ == "__main__":
     m = len(sys.argv) - 1
     if m == 1:
         print("{:d} argument:".format(m))
