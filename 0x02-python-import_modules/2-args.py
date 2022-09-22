776c328fd2614bce239311f786271a126a0cf6e1
LilyPr <lilyeginald09@gmail.com>
2022-09-22 09:31:31 -0700

    number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = 0
+
     m = len(sys.argv) - 1
-    if m == 1:
+    if m == 0:
         print("{:d} argument:".format(m))
-    elif m == 0:
+    elif m == 1:
         print("{:d} arguments.".format(m))
     else:
         print("{:d} arguments:".format(m))
-    for args in sys.argv:
-        if (i != 0):
-            print("{}: {}".format(i, args))
-            i += 1
+    for i in range(m):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
