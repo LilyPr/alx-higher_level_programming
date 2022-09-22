db1d56855a186225fb388ea442b1379bafcf3124
LilyPr <lilyeginald09@gmail.com>
2022-09-22 11:10:28 -0700

    number lists of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 if __name__ == "__main__":
 
-    m = len(sys.argv) - 1
+    m = len(argv) - 1
     if m == 0:
         print("0 arguments.")
     elif m == 1:
         print("1 arguments:")
     else:
         print("{} arguments:".format(m))
-    for i in range(m):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range (m):
+        print("{}: {}".format(i + 1, argv[i + 1]))
