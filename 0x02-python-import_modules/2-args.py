b68a1a2813ffeafa28eb6ebb4abbd5da9660d80e
LilyPr <lilyeginald09@gmail.com>
2022-09-22 10:40:06 -0700

    lists of argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    i = 0
+     """Prints the argument list passed to the program
+
+     The program takes all the arguments starting from the second
+     and prints the number of arguments and their value
+     """
     m = len(sys.argv) - 1
     if m == 0:
         print("0 arguments.")
