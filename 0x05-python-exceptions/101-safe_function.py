e0d220019188fddd7edd852efcb4f4f9122071f4
LilyPr <lilyeginald09@gmail.com>
2022-10-10 04:50:43 -0700

    add files

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sy
+import sys
 
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as ex:
-        print("Exception: {}".format(ex), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return Nones
