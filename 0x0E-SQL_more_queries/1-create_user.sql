38852db19308cfce4254cef8f7f2cf3c6858d259
LilyPr <lilyeginald09@gmail.com>
2022-11-30 13:03:02 -0800

    tests more queries files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
---grant all privileges to a new user in Mysql
+-- grant all privileges to a new user in Mysql
 
-CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'Mypass30@..';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
