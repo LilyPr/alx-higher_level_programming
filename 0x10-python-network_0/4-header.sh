b402f7fcba301432982d45090379963453a4a378
LilyPr <lilyeginald09@gmil.com>
2023-01-17 18:52:32 +0300

    add files

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-School-User-Id-User-Id:98" "$1"
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the respond
+curl -s "{$1}" -X GET -H "X-School-User-Id-User-Id:98" "$1"
